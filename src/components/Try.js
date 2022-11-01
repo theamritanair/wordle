6785da43d04238e4efdf3ed82c400b6be1832b77
Amrita <theamritanair@gmail.com>
2022-11-01 14:09:32 +0530

    No more jumping cells

--- src/components/Try.js
+++ src/components/Try.js
@@ -1,20 +1,25 @@
-import { Cell } from "./Cell"
-export const Try = ({letterLength, attempt, solved, secret}) => {
-    let cells = []
-    for(let i=0;i<letterLength;i++){
-        cells.push(
-            <Cell 
-                key={i}
-                index={i}
-                attempt={attempt}
-                solved={solved}
-                secret={secret}
-            />
-        )
-    }
-    return(
-        <div>
-            {cells}
-        </div>
-    )
-}
+import { Cell } from "./Cell";
+export const Try = ({ letterLength, attempt, solved, secret }) => {
+  let cells = [];
+  for (let i = 0; i < letterLength; i++) {
+    cells.push(
+      <Cell
+        key={i}
+        index={i}
+        attempt={attempt}
+        solved={solved}
+        secret={secret}
+      />
+    );
+  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        "justify-content": "center",
+      }}
+    >
+      {cells}
+    </div>
+  );
+};
