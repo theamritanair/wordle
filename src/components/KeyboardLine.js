84458acb39c6e96e1d4ab9e5edae5cfd5519a74e
Amrita <theamritanair@gmail.com>
2022-11-04 19:30:06 +0530

    Removed logs

--- src/components/KeyboardLine.js
+++ src/components/KeyboardLine.js
@@ -6,18 +6,17 @@ export const KeyboardLine = ({ letters }) => {
   const { currentTry, setCurrentTry } = useContext(SecretContext);
   console.log(currentTry);
   let rows = [];
-  letters.split("").forEach((letter) =>
-    rows.push(
-      <Button
-        handleKeyClick={() => {
-          console.log(currentTry);
-          setCurrentTry(currentTry + letter);
-        }}
-        key={letter}
-        content={letter}
-      />
-    )
-  );
+  letters
+    .split("")
+    .forEach((letter) =>
+      rows.push(
+        <Button
+          handleKeyClick={() => setCurrentTry(currentTry + letter)}
+          key={letter}
+          content={letter}
+        />
+      )
+    );
   return (
     <div style={{ display: "flex", justifyContent: "center" }}>{rows}</div>
   );
